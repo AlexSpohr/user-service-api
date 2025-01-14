4826acb2b8b062bfd16df6c9a44f5a12a4ec3b75
AlexSpohr <alexspohr436@gmail.com>
2025-01-14 13:53:58 -0300

    DEV-000 first commit

---
+++ src/test/java/com/alexspohr/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexspohr.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
